75040e2a82ee9f1552abd6fc543b5934dafb1cce
bernardlcs <bernard.lenin4@gmail.com>
2016-07-30 09:38:56 -0300

    moodificando os controller map

--- src/main/java/bootwildfly/DirlididiController.java
+++ src/main/java/bootwildfly/DirlididiController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DirlididiController {
-
-
-    @RequestMapping("hello")
+	
+	//get
+	@RequestMapping("hello")
     public String sayHello(){
         return ("oi desenvolvedor, seu lindo, bora trabalhar");
     }
@@ -54,19 +54,20 @@ public class DirlididiController {
     	return ("usuarios");
     }
     //post
-    @RequestMapping("/admin/cadastro")
-    public String adminCadastro(@RequestParam(value="name") String name){
+    @RequestMapping("/userCadastro/?name=name$email=email&senha=senha")
+    public String adminCadastro(@RequestParam(value="name") String name,@RequestParam(value="email") String email,@RequestParam(value="senha") String senha){
     	return (name);
     }
-    //Post
-    @RequestMapping("/usuarios/cadastro")
-    public String usuariosCadastro(@RequestParam(value="name") String name){
-    	return (name);
+    
+    //get
+    @RequestMapping("/userlogin/email=email&senha=senha")
+    public String userLogin(@RequestParam(value="email") String email,@RequestParam(value="senha") String senha){
+    	return ("login do usuario");
     }
     //get
-    @RequestMapping("/user/login")
-    public String userLogin(){
-    	return ("login do usuario");
+    @RequestMapping("/ranking")
+    public String ranking(){
+    	return "ranking";
     }
     
 }
